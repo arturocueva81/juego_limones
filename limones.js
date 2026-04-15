1dc8db973b48f05ce25c0e9f4a2369e45e916631
arturocueva81 <ajcs81@hotmail.com>
2026-04-15 12:04:22 -0500

    agrega bajarLimon

--- limones.js
+++ limones.js
@@ -5,12 +5,20 @@ const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
 
+const ANCHO_LIMON=20;
+const ALTURA_LIMON=20;
+
 let personajeX=canvas.width/2;
 let personajeY=canvas.height - (ALTURA_SUELO + ALTURA_PERSONAJE);
+
+let limonX=canvas.width/2;
+let limonY=5;
 
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
+
 }
 
 function dibujarSuelo(){
@@ -39,11 +47,16 @@ function actualizarPantalla(){
     limpiarCanva();
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 
 }
 
 function reiniciarJuego(){
     personajeX=canvas.width/2;
+    personajeY=canvas.height - (ALTURA_SUELO + ALTURA_PERSONAJE);
+
+    limonX=canvas.width/2;
+    limonY=5;
     dibujarSuelo();
     dibujarPersonaje();
     actualizarPantalla();
@@ -51,4 +64,14 @@ function reiniciarJuego(){
 
 function limpiarCanva(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function dibujarLimon(){
+    ctx.fillStyle="green";
+    ctx.fillRect(limonX, limonY, ANCHO_LIMON, ALTURA_LIMON);
+}
+
+function bajarLimon(){
+    limonY=limonY + 10;
+    actualizarPantalla();
 }
